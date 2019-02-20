6c053b1d87bf3eaec9eaed90d2633b0081764443
Laurie <lau@voodoobytes.co.uk>
2019-02-20 18:03:43 +0000

    Add `ConfigureIndexing.cs` event handler to add things into the Examine Index

---
+++ Src/UmbracoLookWineDemo.Logic/Events/ConfigureIndexing.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Our.Umbraco.Look;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace UmbracoLookWineDemo.Logic.Events
+{
+    public class ConfigureIndexing : ApplicationEventHandler
+    {
+        /// <summary>
+        /// Umbraco has started event
+        /// </summary>
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            LookConfiguration.TextIndexer = indexingContext => { 
+
+                // eg. if content, render page and scrape markup
+                if (!indexingContext.IsDetached && indexingContext.Item.ItemType == PublishedItemType.Content) 
+                {				
+                    // (could pass in httpContext to render page without http web request)
+                    // return string
+                }
+
+                return null; // don't index
+            };
+            
+            LookConfiguration.TagIndexer = indexingContext =>
+            {
+                var tags = new List<LookTag>();
+
+                if (indexingContext.Item.DocumentTypeAlias == "wineReview")
+                {
+                    // add tags 
+                    tags.Add(new LookTag("designation:" + indexingContext.Item.GetPropertyValue<string>("designation")));
+                }
+                return tags.ToArray();
+            };
+            
+        }
+    }
+}
